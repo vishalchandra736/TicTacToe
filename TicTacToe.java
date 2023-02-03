7c084deb9aa8f96cc5235b898c2a3ac9b27b49c2
vishalchandra736 <vishalchandra736@gmail.com>
2023-02-04 01:20:56 +0530

    Added : Display new board

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,16 @@
 package com.blz.tictactoe;
 
 public class TicTacToe {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe");
+    public void GameBoard() {
+        char board[] = new char[10];
+        for (int index=1; index<=9; index++) {
+            board[index]=' ';
+        }
+
+    }
+    public static void main(String args[]) {
+        System.out.println("Welcome to tic tac toe");
+        TicTacToe obj = new TicTacToe();
+        obj.GameBoard();
     }
 }
